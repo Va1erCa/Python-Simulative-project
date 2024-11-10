240700dd39bc3f40353ae56cd6df8e72052351e3
Valery Zhidkow <vjpaps793@gmail.com>
2024-11-11 00:58:58 +0300

    Completed api module

--- config.py
+++ config.py
@@ -24,6 +24,9 @@ REQ_CLIENT_KEY = env('client_key', str)
 REQ_START_TIME = datetime.fromisoformat('2023-04-01 00:00:00.000000')
 REQ_END_TIME = datetime.fromisoformat('2023-04-01 00:01:59.999999')
 
+# Test or normal app operation mode
+TEST_APP_MODE = False
+
 # Logging system's parameters
 LOGS_PATH = '.logs'         # Log's files path
 MAX_NUM_LOGS_FILES = 3      # Maximum enabled number of log's files  ( acceptable >= 1 )
@@ -31,36 +34,39 @@ MAX_NUM_LOGS_FILES = 3      # Maximum enabled number of log's files  ( acceptabl
 # Parameters for connection to local Postgres server
 SERVER_CONNECTION_PARAMS = dict(host = 'localhost',
                                 port = 5432,
-                                database = 'postgres',
+                                dbname = 'postgres',
                                 user = 'postgres',
                                 password = '123456',
                                 autocommit = False
                                 )
 
-# SERVER_PARAM_AUTOCOMMIT = False
-
-
 # Language support
 RUSSIAN = 0
 ENGLISH = 1
-CURR_LANG = ENGLISH  # selected language
+CURR_LANG = RUSSIAN  # selected language
 
 # Language package of service messages
 #
 # data_processor module
 AN_INVITATION_FOR_ENTERING_PROCESSING_DATE = (
-                                'Введите дату извлечения данных в формате: <YYYY-mm-dd> или <exit> для выхода: ',
-                                'Enter processing date in format: <<YYYY-mm-dd> or type <exit> for quit: ')
+                            'Введите дату извлечения/загрузки данных в формате: <YYYY-mm-dd> или <exit> для выхода: ',
+                            'Enter processing date in format: <<YYYY-mm-dd> or type <exit> for quit: ')
 MESSAGE_ABOUT_BAD_INPUT = ('Ошибка ввода даты, попробуйте еще раз...', 'Bad input. Let\'s try again...')
 
 # Logger's messages texts
-LOG_STARTING_WORK_WITH_API = ('скачивание данных началось', 'beginning getting of data')
-LOG_ENDING_WORK_WITH_API = ('скачивание данных завершено', 'ending getting of data')
+LOG_STARTING_WORK_WITH_API = (('начинаем сбор данных за ', ' с: ', ' по: '),
+                              ('we start collecting data for ', ' from: ', ' to: '))
+LOG_ENDING_WORK_WITH_API = ('завершено скачивание данных за ', 'data download completed for ')
+
+LOG_TOTAL_NUM_LINES_API = (('общее количество прочитанных строк: ', ', в том числе ', ' успешно, ', ' испорчено'),
+                           ('total number of lines read: ', ' including ', ' successfully, ', ' is corrupted'))
 
 LOG_STARTING_WORK_WITH_DBMS = ('загрузка данных в базу началась', 'the upload to the database has started')
 LOG_ENDING_WORK_WITH_DBMS = ( 'загрузка данных в базу завершена', 'the upload to the database has ended')
 
-
+LOG_IT_WAS_READ = (('строка № ', ' прочитана: '), ('line # ', ' was read: '))
+LOG_ERROR_READ = (('строка № ', ' не прочитана - ошибка - отсутствует идентификатор <lti_user_id>'),
+                  ('line # ', ' wasn\'t read - error - the <lti_user_id> id is missing: '))
 
 
 if __name__ == '__main__' :
--- get_data.py
+++ get_data.py
@@ -8,6 +8,7 @@ import requests, json, logging
 
 # App's modules
 import config
+from config import CURR_LANG
 
 start = datetime
 end = datetime
@@ -24,17 +25,37 @@ class Row :
 
 def get_rows(logger: logging.Logger | None = None,
              start_time: datetime | None = None,
-             end_time: datetime | None = None) -> list[Row] :
+             end_time: datetime | None = None) -> tuple[list[Row], int, int] :
+
     # Initializing request parameters
     params = {'client': config.REQ_CLIENT,
               'client_key': config.REQ_CLIENT_KEY,
               'start': start_time if start_time else config.REQ_START_TIME,
               'end': end_time if end_time else config.REQ_END_TIME
               }
+    res = []
     # executing the request
     try :
         r = requests.get(config.API_URL, params=params)
         r.raise_for_status()    # raise exception if status of request aren't the "ok"
+        corrupt_read = 0
+        for i, line in enumerate(r.json(), 1) :
+
+            if line['lti_user_id'] is not None:
+                res.append(Row(lti_user_id = line.get('lti_user_id', None),
+                               passback_params = eval(line.get('passback_params', '{}')),
+                               is_correct = line.get('is_correct', None),
+                               attempt_type = line.get('attempt_type', None),
+                               created_at = line.get('created_at', None)
+                               ))
+
+                logger.info(f'{config.LOG_IT_WAS_READ[CURR_LANG][0]}{i}'
+                            f'{config.LOG_IT_WAS_READ[CURR_LANG][1]}{res[-1]}')
+
+            else :
+                logger.error(f'{config.LOG_ERROR_READ[CURR_LANG][0]}{i}'
+                            f'{config.LOG_ERROR_READ[CURR_LANG][1]}')
+                corrupt_read += 1
 
     # work with exceptions
     except requests.HTTPError as e :
@@ -42,15 +63,14 @@ def get_rows(logger: logging.Logger | None = None,
             logger.error(f'HTTP Error: {e}')
         else:
             print(f'HTTP Error: {e}')
-        return []
+
     except requests.RequestException as e :
         if logger:
             logger.error(f'Request Error: {e}')
         else:
             print(f'Request Error: {e}')
-        return []
 
-    return r.json()
+    return res, len(r.json()), corrupt_read
 
 
 if __name__ == '__main__' :
